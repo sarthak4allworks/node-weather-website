0d1280866982612ccf8dd27e6c5e240808e3e9ca
SARTHAK AGARWAL <agarwalsarthak21@gmail.com>
2020-10-31 17:23:04 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ const messageTwo = document.querySelector('#forecast');
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
-    const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
     fetch(url).then((response) => {
